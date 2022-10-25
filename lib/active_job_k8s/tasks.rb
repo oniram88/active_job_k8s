4f951a1e76fc216e64cb313383cd878221740e89
Marino Bonetti <marinobonetti@gmail.com>
2022-10-25 22:19:39 +0200

    complete setup to generate task

--- lib/active_job_k8s/tasks.rb
+++ lib/active_job_k8s/tasks.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 namespace :active_job_k8s do
+
+  desc "Execute the job from ENV: SERIALIZED_JOB"
   task run_job: :environment do
     ActiveJobK8s::Scheduler.execute_job
   end
